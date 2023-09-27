191de169a18cda4b099ef7c95f53d902e61886a6
Pierson Browne <pbrowne88@gmail.com>
2023-09-27 13:26:23 -0400

    2023-09-27 1pm

--- rrps_contracts/contracts/rrps.sol
+++ rrps_contracts/contracts/rrps.sol
@@ -1,29 +1,152 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.9;
 
-import {Card, Star} from "./items.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+
+contract RRPS is ERC1155, Ownable {
+    constructor() ERC1155("") {}
+
+    uint public constant STAR = 0;
+    uint public constant ROCK = 1; 
+    uint public constant PAPER = 2;
+    uint public constant SCISSORS = 3;
+
+    mapping(address => bool) players;
+    mapping(string => address) nicknamesToAddresses;
+    mapping(address => string) addressesToNicknames;
+    // string[] nicknameList;
+
+    function startGame(string memory nickname) public /* payable */ {
+        // Check payment
+        // require(msg.value >= 0.1 ether);
+        // Ensure that nickname is unique
+        require(nicknamesToAddresses[nickname] == address(0), "Nickname already exists");
+        // Ensure that address is not currently playing the game
+        require(players[msg.sender] == false, "Address is already playing.");
+        // Log the address as playing the game
+        players[msg.sender] = true; 
+        // Associate the nickname 
+        nicknamesToAddresses[nickname] = msg.sender;
+        addressesToNicknames[msg.sender] = nickname;
+        // nicknameList.push(nickname);
+
+        // Mint new set of cards
+        mint(msg.sender, ROCK, 4, "");
+        mint(msg.sender, PAPER, 4, "");
+        mint(msg.sender, SCISSORS, 4, "");
+        
+        // Mint new set of stars
+        mint(msg.sender, STAR, 3, "");
+    }
 
 
-contract RRPS {
+    function gameOver(address player) internal {
+        //Double check that player has no stars remaining
+        require(balanceOf(player, STAR) == 0, "Player still has stars remaining");
 
-  mapping(address => bool) players;
-  mapping(string => address) nicknames;
+        //Burn remaining cards
+        _burn(player, ROCK, balanceOf(player, ROCK));
+        _burn(player, PAPER, balanceOf(player, PAPER));
+        _burn(player, SCISSORS, balanceOf(player, SCISSORS));
 
-  Star starNFT = new Star();
-  Card cardNFT = new Card();
+        //De-register address and nickname
+        nicknamesToAddresses[addressesToNicknames[player]] = address(0);
+        addressesToNicknames[player] = "";
+        players[player] = false;
+    }
 
-  function dealIn(address playerAddress) public {
-    
-    for (uint i=0; i <= 3; i++){
-      starNFT.createStars(playerAddress);
+    struct challenge {
+        address challenger;
+        address challengee;
+    }
+
+    // Nested mapping allows rapid identification of who is challenging whom
+    mapping (address => mapping (address => bool)) challenges; 
+
+
+    function challengeCommit(address challengee) public view {
+        // Check that challenger has at least one star and at least one card remaining
+        require(balanceOf(msg.sender, STAR) > 0, "Requires that you have at least one star.");
+        require(totalCards(msg.sender) > 0, "Requires that you have at least one card to challenge with.");
+        // Check that challengee has at least one star and at least one card remaining
+        require(balanceOf(challengee, STAR) > 0, "Requires that the challengee has at least one star.");
+        require(totalCards(challengee) > 0);
+        // Check that challenger has at least one card not already committed to a challenge
+        // Check that challengee doesn't have an open challenge against challenger
+
 
     }
-  }
 
-  function startGame(string memory nickname) payable public {
-    require(players[msg.sender] == false);
-    players[msg.sender] = true;
-    nicknames[nickname] = msg.sender;
-    dealIn(msg.sender);
-  }
+    function openCommit(uint cardType) public{
+        
+
+    }
+
+    function reveal() public {
+
+    }
+
+    function _burnOneStar() public returns(uint) {
+        require(balanceOf(msg.sender, STAR) > 0, "Requires at least one star.");
+        _burn(msg.sender, STAR, 1);
+        if (balanceOf(msg.sender, STAR) <= 0){
+            gameOver(msg.sender);
+        }
+        return(balanceOf(msg.sender, STAR));
+    }
+
+
+
+    // event newPlayer()
+
+    function contractBalance() public view returns(uint256){
+        return(address(this).balance);
+    }
+
+    function totalCards(address player) public view returns (uint256) {
+        return(
+            balanceOf(player, ROCK) + 
+            balanceOf(player, PAPER) + 
+            balanceOf(player, SCISSORS)
+        );
+    }
+
+    function balanceOf() public view returns(uint256, uint256, uint256, uint256) {
+        return(balanceOf(msg.sender));
+    }
+    
+    function balanceOf(string memory nickname) public view returns(uint256, uint256, uint256, uint256){
+        return(balanceOf(nicknamesToAddresses[nickname]));
+    }
+    
+    function balanceOf(address account) public view returns(uint256, uint256, uint256, uint256) {
+        return (
+            balanceOf(account, 0),
+            balanceOf(account, 1),
+            balanceOf(account, 2),
+            balanceOf(account, 3)
+        );
+    }
+
+    function balanceOf(string memory nickname, uint256 id) public view {
+        balanceOf(nicknamesToAddresses[nickname], id);
+    }
+
+    function setURI(string memory newuri) internal {
+        _setURI(newuri);
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        internal
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
 }
